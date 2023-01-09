dd9fbd89fcdd6c9a51f87c9cec4ed6f7116927e2
Unno Wataru <unnow@networld.co.jp>
2023-01-10 00:46:23 +0900

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,5 @@
+resource "azurerm_subscription" "example" {
+  alias             = "examplesub"
+  subscription_name = "Azure subscription 1"
+  subscription_id   = "50f6f1db-f51f-4be3-bfd3-f594a52c9ebd"
+}
