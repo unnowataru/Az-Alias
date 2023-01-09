638b9be99213b73899ca80b9b2b9e0c526bdccdc
Unno Wataru <unnow@networld.co.jp>
2023-01-10 01:13:49 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+variable "usr_subscription_id" {}       # サブスクリプションID (調べる)
+variable "usr_client_id" {}             # クライアントID (アプリを登録したときに自動生成される)
+variable "usr_client_secret" {}         # シークレット/パスワード (サービスプリンシパルを作ったときに入力している)
+variable "usr_tenant_id" {}             # テナントID (調べる)
+
 provider "azurerm" {
   features {}
   subscription_id = var.usr_subscription_id
